0765f215741b18be69015f46de3320fd23e4f3d3
harshal <harshal.sonawane21@pccoepune.org>
2023-06-13 18:56:21 +0530

    just commit

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,7 @@ async function postData(url = "", data = {}) {
     const response = await fetch(url, {
       method: "POST", headers: {
         "Content-Type": "application/json", 
-      }, body: JSON.stringify(data),  
+      }, body: JSON.stringify(data) ,  
     });
     return response.json(); 
   }
